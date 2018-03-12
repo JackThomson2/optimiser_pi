89f2280849245fdd817cfdb780d7b6680a5953b3
Jack Thomson <jackathomson@outlook.com>
2018-03-12 14:47:02 +0000

    Allow reader to be stored for future access

--- reader.py
+++ reader.py
@@ -2,6 +2,7 @@ import time
 import threading
 import json
 import math
+import datetime, time
 from bluetooth import *
 from sensor import mpu6050
 from multi import I2C_controller, PTR_ARR
@@ -16,13 +17,21 @@ class DataStore:
         self.controller = I2C_controller()
         self.sensor = mpu6050(0x68)
         self.sensor.set_accel_range(int(mpu6050.ACCEL_SCALE_MODIFIER_2G))
+        self.recording_time = 0
+        self.recording_name = 'NONE'
     
     # This method is used to start the recording of the data from the sensors
     def log_data(self, stop_event):
+        start = time.time()
+
+        d_t = datetime.datetime.utcnow()
+        self.recording_name = str(int(time.mktime(d_t.timetuple())) * 1000)
+
         while not stop_event.is_set():
             self.data.append(self.get_all_sensors())
             self.cntr += 1
             time.sleep(0.05)
+        self.recording_time = time.time() - start
 
     # This method will call the multiplexer to switch between all the accelerometers
     # data is returned as an array of objects
@@ -40,6 +49,10 @@ class DataStore:
             except:
                 print(f"Error with {i}")
         return return_arr
+
+    # Used for the managing object to pull data and store
+    def get_storage(self):
+        return {'name': self.recording_name, 'duration': self.recording_time, 'data': self.data}
 
     # Dumps all the data into JSON
     def get_dump(self):
